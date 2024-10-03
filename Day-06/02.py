e06a3ecb6cdc8082aa538bccdac5a6b0dec254ed
dhruvil-84 <150236262+dhruvil-84@users.noreply.github.com>
2024-10-03 19:50:32 +0530

    python functions and karel

---
+++ Day-06/02.py
@@ -0,0 +1,27 @@
+'''
+for vs while loop
+
+for loop:
+--> Entry Controlled Loop.
+--> used when we know how many times we have to iterate.
+--> use forloop when you want to iterate over an iterable.
+
+while loop:
+--> Entry Controlled Loop.
+--> used when we dont know how many times we have to iterate but we know a condition till when we have to iterate.
+--> use whileloop when you dont care what number in a sequence you are in, which item you are iterating through in a list and you just simply want to carry out some sort of functionality many, many times until some sort of condition you set.
+
+Syntax:
+while some_condition_is_true:
+	# Do this...
+	# Then do this...
+	# Finally do this...
+ 
+Tabs or Spaces?
+    Python document says to use 4 spaces instead of 1 Tab for Indentation.
+    why so?
+    --> Spaces are the preferred indentation method.
+    --> Tabs should be used solely to remain consistent with code that is already indented with tabs.
+    --> Python disallows mixing tabs and spaces for indentation.
+
+'''
