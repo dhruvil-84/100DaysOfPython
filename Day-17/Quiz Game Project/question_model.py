ea1e1618c1a4c8eb6eddf3e93ab1131c52f945ad
dhruvil-84 <150236262+dhruvil-84@users.noreply.github.com>
2024-10-08 08:23:26 +0530

    Delete Day-17 directory

--- Day-17/Quiz Game Project/question_model.py
+++
@@ -1,8 +0,0 @@
-class Question:
-    def __init__(self, txt, ans):
-        self.text = txt
-        self.answer = ans
-
-# new_q = Question("2 + 3 = 5", "True")
-# print(new_q.text)
-# print(new_q.answer)
